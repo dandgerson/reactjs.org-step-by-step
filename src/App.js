7bbcc6e4a0626ea33d0a452858f09cf80f62444b
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-14 20:41:29 +0300

    up

--- src/App.js
+++ src/App.js
@@ -21,11 +21,71 @@ export default class App extends React.Component {
   }
 
   render() {
+    const element = <Welcome name={this.formatName(this.props.user)} />
+    const names = ['Dmitry', 'Yegor', 'Yaroslav', 'Lada'];
+
     return (
       <div className="app">
         {this.getGreeting(this.props.user)}
+
         <Clock />
+
+        {element}
+        <Welcome name={this.formatName(this.props.user)} />
+
+        <ul>
+          {names.map((name, i) => <li key={i}><Welcome name={name} /></li>)}
+        </ul>
+
+        <Comment
+          author={this.props.user}
+          text={this.props.user.text}
+          date={this.props.user.date}
+        />
       </div>
     );
   }
+}
+
+// function Component
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author}/>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formateDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="author-avatar"
+      src={props.user.avatarURL}
+      alt={props.user.firstName}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.firstName + ' ' + props.user.lastName}
+      </div>
+    </div>
+  );
+}
+
+function formateDate(date) {
+  return date.toLocaleString('ru');
 }
