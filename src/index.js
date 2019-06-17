29f041f260b90c19c258f756cdbae97597ff13d1
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-17 20:15:09 +0300

    up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css'
+import './scss/custom.scss';
+
 import './index.scss';
 
 import * as data from './data';
