43d25c07580598145695979288a5380dcae95221
Dmitry G. Anderson <dandgerson@gmail.com>
2019-09-10 11:34:06 +0300

    refactor scss structure

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+class MiniFormik extends React.Component {
+
+  state = {};
+
+  render() {
+    return this.props.children({...this.state});
+  }
+}
+
 class Reservation extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +22,7 @@ class Reservation extends React.Component {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
-
+ 
     this.setState({
       [name]: value,
     });
@@ -23,28 +32,30 @@ class Reservation extends React.Component {
     return (
       <div className="row">
         <div className="col">
-          <form>
-            <div className="row">
-              <div className="col">
-                <label>
-                  Is going:
-                  <input className="form-control" name="isGoing"
-                    type="checkbox"
-                    checked={this.state.isGoing}
-                    onChange={this.handleInputChange} />
-                </label>
+          <MiniFormik>
+            {() => <form>
+              <div className="row">
+                <div className="col">
+                  <label>
+                    Is going:
+                    <input className="form-control checkbox" name="isGoing"
+                      type="checkbox"
+                      checked={this.state.isGoing}
+                      onChange={this.handleInputChange} />
+                  </label>
+                </div>
+                <div className="col">
+                  <label>
+                    Number of guests:
+                    <input className="form-control" name="numberOfGuests"
+                      type="number"
+                      value={this.state.numberOfGuests}
+                      onChange={this.handleInputChange} />
+                  </label>
+                </div>
               </div>
-              <div className="col">
-                <label>
-                  Number of guests:
-                  <input className="form-control" name="numberOfGuests"
-                    type="number"
-                    value={this.state.numberOfGuests}
-                    onChange={this.handleInputChange} />
-                </label>
-              </div>
-            </div>
-          </form>
+            </form>}
+          </MiniFormik>
         </div>
       </div>
     );
