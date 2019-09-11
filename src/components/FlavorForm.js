1e0b679c13072e0b16a5687c784f7f7678e8c762
Dmitry G. Anderson <dandgerson@gmail.com>
2019-09-11 14:49:50 +0300

    ligth up multiple select

--- src/components/FlavorForm.js
+++ src/components/FlavorForm.js
@@ -4,16 +4,30 @@ class FlavorForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {value: 'coconut'};
+    this.state = { values: [] };
   }
 
-  handleChange = event => {
-    this.setState({value: event.target.value});
+  handleChange = (event) => {}
+
+  handleClick = (event) => {
+    const value = event.target.value
+    console.log(value)
+    this.setState(prevState => {
+      const selected = prevState.values.slice()
+      console.log(selected)
+      return (selected.includes(value))
+      ? { values: selected.filter(item => item !== value) }
+      : { values: [
+          ...selected,
+          value,
+        ],
+      }
+    });
   }
 
   handleSumbit = event => {
     console.log(
-      'Your favorite flavor is: ' + this.state.value,
+      'Your favorite flavor is: ' + this.state.values,
       '(json data updated)'
     );
     event.preventDefault();
@@ -29,9 +43,11 @@ class FlavorForm extends React.Component {
                 <div className="col d-flex align-items-center">
                   <label>
                     Pick your favorite flavor:
-                    <select value={this.state.value}
+                    <select multiple={true} value={this.state.values}
                       className="form-control"
-                      onChange={this.handleChange}>
+                      onChange={this.handleChange}
+                      onClick={this.handleClick}
+                    >
                       <option value="grapefruit">Grapefruit</option>
                       <option value="lime">Lime</option>
                       <option value="coconut">Coconut</option>
